1e575c8b5efd0e6e3be3be9a97fd9e3009ef252e
ALEX-KGN <99672467+ALEX-KGN@users.noreply.github.com>
2023-12-03 18:16:32 +0300

    Add files via upload

---
+++ PSP5_2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Reader prod = new Reader("Прием продукции");
+        prod.setVisible(true);
+        prod.setResizable(false);
+        prod.setLocationRelativeTo(null);
+    }
+}
